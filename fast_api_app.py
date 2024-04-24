4a4f738c770755ddcfc428c99615fd5bfef8f9c1
illTushar <gtushar7668@gmail.com>
2024-04-24 23:26:41 +0530

    first commit

---
+++ fast_api_app.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from request.product import *
+
+app = FastAPI()
+
+
+# path parameter
+@app.get("/item/{id}")
+async def item_id(id: int):
+    return {"message": f"This item_id is {id}"}
+
+
+# query parameter
+@app.get("/user/product")
+async def product(id: int, price: int, name: str | None = None):
+    if name:
+        return {"message": f"The product_id is {id} and it's price is {price} , brand name is {name} "}
+
+    else:
+        return {"message": f"The product_id is {id} and it's price is {price}"}
+
+
+# request body parameter
+@app.post("/product")
+async def item_details(item: Item):
+    item_new_key = item.dict()
+
+    if item.price is not None and item.gst is not None:
+        include_gst_price = item.price + item.gst
+        item_new_key["gst_include_price"] = include_gst_price
+
+    if "gst_include_price" in item_new_key:
+        return item_new_key
+    else:
+        return "Not Present"
+
+
+@app.post("/product/{product_id}")
+def product_description(item: Item, user: User, product_id: int, product_name: str):
+    return {"user": user, "product_id": product_id, "product_name": product_name, "item": item}
---
+++ request/product.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, Field
+
+
+class Item(BaseModel):
+    name: str
+    # | None = None make parameter optional.
+    buyer_name: str | None = None
+    price: int
+    vat: int | None = None
+    gst: int
+
+
+class User(BaseModel):
+    name: str = Field(title="You can enter your name", description="The name field is requireds", gt=0)
+    email: str
